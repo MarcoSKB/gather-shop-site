4e2359f7305132e8312f5845f0b89a5a26f0ca59
Marco <serikbaev03beka@gmail.com>
2026-01-30 01:12:31 +0500

    feat(routes): add pages and routes

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,8 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@pages/Home/HomePage.vue'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: HomeView,
+  },
+  {
+    path: '/cart',
+    component: () => import('@pages/Cart/CartPage.vue'),
+  },
+  {
+    path: '/post',
+    component: () => import('@pages/Post/PostPage.vue'),
+  },
+  {
+    path: '/product',
+    component: () => import('@pages/Product/ProductPage.vue'),
+  },
+  {
+    path: '/reading',
+    component: () => import('@pages/Reading/ReadingPage.vue'),
+  },
+  {
+    path: '/shop',
+    component: () => import('@pages/Shop/ShopPage.vue'),
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
