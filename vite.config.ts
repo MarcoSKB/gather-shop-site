720e4b382444b8ec6387582393aa20c116007b23
Marco <serikbaev03beka@gmail.com>
2026-01-29 22:03:17 +0500

    [feat]: add fonts, new aliases

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,14 @@ export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
+      '@': fileURLToPath(new URL('./', import.meta.url)),
       '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
       '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
       '@widgets': fileURLToPath(new URL('./src/widgets', import.meta.url)),
       '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
       '@entities': fileURLToPath(new URL('./src/entities', import.meta.url)),
       '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./public/assets', import.meta.url)),
     },
   },
 })
