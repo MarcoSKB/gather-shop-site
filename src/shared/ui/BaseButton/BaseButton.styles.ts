0bdbfb4a357ccb02a028646712f1ec44e766021c
Marco <serikbaev03beka@gmail.com>
2026-01-31 16:10:21 +0500

    feat(ui): add BaseButton.vue

---
+++ src/shared/ui/BaseButton/BaseButton.styles.ts
@@ -0,0 +1,20 @@
+import { cva } from 'class-variance-authority'
+
+export const buttonStyles = cva(
+  'font-poppins box-border flex w-full cursor-pointer items-center justify-center text-base tracking-[2%] transition-colors duration-250 ease-in disabled:cursor-default',
+  {
+    variants: {
+      variant: {
+        primary:
+          'text-primary border-primary hover:bg-primary hover:text-inverse border border-solid',
+        secondary:
+          'bg-primary text-inverse hover:bg-inverse border-primary boder-solid hover:text-primary border',
+      },
+      size: {
+        md: 'px-6 py-2',
+        lg: 'p-6',
+      },
+    },
+    defaultVariants: { variant: 'primary', size: 'md' },
+  },
+)
---
+++ src/shared/ui/BaseButton/BaseButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { ButtonHTMLAttributes } from 'vue'
+import { buttonStyles } from './BaseButton.styles'
+
+const props = withDefaults(
+  defineProps<
+    {
+      variant?: 'primary' | 'secondary'
+      size?: 'md' | 'lg'
+    } & /* @vue-ignore */ ButtonHTMLAttributes
+  >(),
+  {
+    type: 'button',
+    variant: 'secondary',
+    size: 'md',
+  },
+)
+
+const { variant, size, ...buttonProps } = props
+</script>
+
+<template>
+  <button v-bind="buttonProps" :class="buttonStyles({ variant, size })">
+    <slot />
+  </button>
+</template>
