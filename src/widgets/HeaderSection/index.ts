0285b42cd3fa4ada0456382b6f42502e5e690de0
Marco <serikbaev03beka@gmail.com>
2026-02-04 19:59:44 +0500

    feat(ui): Add HeaderSection component

---
+++ src/widgets/HeaderSection/index.ts
@@ -0,0 +1,1 @@
+export { default as HeaderSection } from './ui/HeaderSection.vue'
---
+++ src/widgets/HeaderSection/ui/HeaderSection.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { BaseContainer } from '@/src/shared/ui/BaseContainer'
+import { ButtonIcon } from '@/src/shared/ui/ButtonIcon'
+import { RouterLink } from '@/src/shared/ui/RouterLink'
+import { Menu, Search, ShoppingBag } from 'lucide-vue-next'
+import { RouterLink as VueRouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <BaseContainer class="flex-row justify-between py-6">
+      <div class="flex w-full max-w-[30%] items-center gap-8">
+        <div class="max-w-32"><BaseButton variant="primary">Subscribe</BaseButton></div>
+        <RouterLink to="/">Issue 49</RouterLink>
+        <RouterLink to="/shop">Shop</RouterLink>
+      </div>
+      <VueRouterLink to="/" title="To Home page">
+        <img src="/assets/images/logo.svg" alt="Elmore logotype" class="w-75" />
+      </VueRouterLink>
+      <div class="flex w-full max-w-[30%] items-center justify-end gap-10">
+        <ButtonIcon title="Cart">
+          <ShoppingBag />
+        </ButtonIcon>
+        <ButtonIcon title="Search">
+          <Search />
+        </ButtonIcon>
+        <ButtonIcon title="Menu">
+          <Menu />
+        </ButtonIcon>
+      </div>
+    </BaseContainer>
+  </header>
+</template>
