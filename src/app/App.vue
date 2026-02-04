c0699ea833776ee7ccfaa9c7e26036cc7b24e6ad
Marco <serikbaev03beka@gmail.com>
2026-02-04 20:00:03 +0500

    feat(ui): Add header and footer sections

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import { FooterSection } from '@widgets/FooterSection'
+import { HeaderSection } from '@widgets/HeaderSection'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <HeaderSection />
   <RouterView />
+  <FooterSection />
 </template>
