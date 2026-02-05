9d30fe070eebc8c71a08fad754d8bda4271b93bd
Marco <serikbaev03beka@gmail.com>
2026-02-05 19:36:55 +0500

    feat(ui): Add class handling for button icon

--- src/shared/ui/ButtonIcon/ButtonIcon.vue
+++ src/shared/ui/ButtonIcon/ButtonIcon.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import type { ButtonHTMLAttributes } from 'vue'
+import clsx from 'clsx'
+import { computed, type ButtonHTMLAttributes } from 'vue'
 import { buttonStyles, type ButtonVariantsType } from './ButtonIcon.styles'
 
 type Props = {
+  class?: string
   type?: ButtonHTMLAttributes['type']
   variant?: ButtonVariantsType['variant']
   size?: ButtonVariantsType['size']
@@ -13,10 +15,11 @@ const props = withDefaults(defineProps<Props>(), {
 })
 
 const { variant, size, ...buttonProps } = props
+const buttonClasses = computed(() => clsx(buttonStyles({ variant, size }), props.class))
 </script>
 
 <template>
-  <button v-bind="buttonProps" :class="buttonStyles({ variant, size })">
+  <button v-bind="buttonProps" :class="buttonClasses">
     <slot />
   </button>
 </template>
