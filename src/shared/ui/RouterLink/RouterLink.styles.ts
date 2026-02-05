33ccea56f0a6aa8473277072ca5cc3b7fb786114
Marco <serikbaev03beka@gmail.com>
2026-02-05 23:20:14 +0500

    refactor(ui): Add secondary variant and size support

--- src/shared/ui/RouterLink/RouterLink.styles.ts
+++ src/shared/ui/RouterLink/RouterLink.styles.ts
@@ -1,16 +1,19 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 
-export const linkStyles = cva(
-  'font-poppins text-nowrap aria-[current="page"]:pointer-events-none',
-  {
-    variants: {
-      variant: {
-        primary:
-          'text-primary before:bg-primary relative z-0 py-1.5 transition-opacity before:absolute before:bottom-2 before:left-0 before:h-px before:w-[0%] before:transition-all before:duration-300 before:content-[""] hover:before:w-full active:opacity-50 aria-[current="page"]:before:w-full',
-      },
+export const linkStyles = cva('text-nowrap aria-[current="page"]:pointer-events-none', {
+  variants: {
+    variant: {
+      primary:
+        'font-poppins text-primary before:bg-primary relative z-0 py-1.5 transition-opacity before:absolute before:bottom-2 before:left-0 before:h-px before:w-[0%] before:transition-all before:duration-300 before:content-[""] hover:before:w-full active:opacity-50 aria-[current="page"]:before:w-full',
+      secondary:
+        'font-spectral text-primary transition-opacity duration-150 ease-in hover:opacity-60',
     },
-    defaultVariants: { variant: 'primary' },
+    size: {
+      md: 'text-base',
+      lg: 'text-2xl',
+    },
   },
-)
+  defaultVariants: { variant: 'primary', size: 'md' },
+})
 
 export type LinkStylesType = VariantProps<typeof linkStyles>
--- src/shared/ui/RouterLink/RouterLink.vue
+++ src/shared/ui/RouterLink/RouterLink.vue
@@ -4,12 +4,13 @@ import { linkStyles, type LinkStylesType } from './RouterLink.styles'
 
 defineProps<{
   variant?: LinkStylesType['variant']
+  size?: LinkStylesType['size']
   to: string
 }>()
 </script>
 
 <template>
-  <RouterLink :to :class="linkStyles({ variant })">
+  <RouterLink :to :class="linkStyles({ variant, size })">
     <slot />
   </RouterLink>
 </template>
