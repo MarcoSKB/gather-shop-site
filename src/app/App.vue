0531e3002826e87d7f0f1050d2c34a5a2913c6cf
Marco <serikbaev03beka@gmail.com>
2026-02-05 23:22:53 +0500

    feat(ui): Add GSAP initialization and adjust padding

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { initGsap } from '@shared/lib/gsap'
 import { FooterSection } from '@widgets/FooterSection'
 import { HeaderSection } from '@widgets/HeaderSection'
 import { RouterView } from 'vue-router'
+
+initGsap()
 </script>
 
 <template>
-  <div class="flex min-h-dvh flex-col">
+  <div class="flex min-h-dvh flex-col pt-16 md:pt-0">
     <HeaderSection />
     <RouterView />
     <FooterSection />
