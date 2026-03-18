e262afa1ee351be039dced4d9a295b78a34d5c26
Marco <serikbaev03beka@gmail.com>
2026-03-19 01:51:51 +0500

    refactor(ui): Update imports and router links

--- src/widgets/HeaderSection/ui/HeaderSection.vue
+++ src/widgets/HeaderSection/ui/HeaderSection.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-import { BaseButton } from '@/src/shared/ui/BaseButton'
-import { BaseContainer } from '@/src/shared/ui/BaseContainer'
-import { ButtonIcon } from '@/src/shared/ui/ButtonIcon'
-import { RouterLink } from '@/src/shared/ui/RouterLink'
+import { BaseButton, BaseContainer, ButtonIcon, RouterLink } from '@shared/ui'
 import { Menu, Search, ShoppingBag } from 'lucide-vue-next'
 import { ref } from 'vue'
-import { RouterLink as VueRouterLink } from 'vue-router'
 import { useLogoAnimation } from '../lib/useLogoAnimation'
 import { useHeaderMenu } from '../model/useHeaderMenu'
 import HeaderMenuOverlay from './HeaderMenuOverlay.vue'
@@ -21,13 +17,14 @@ const { isOpen, menuRef, toggle, close } = useHeaderMenu()
     <BaseContainer class="flex-row justify-between py-3 md:py-6">
       <div class="hidden w-full max-w-[33%] items-center gap-8 md:flex">
         <BaseButton variant="primary" class="hidden max-w-32 lg:flex">Subscribe</BaseButton>
-        <RouterLink to="/">Issue 49</RouterLink>
-        <RouterLink to="/shop">Shop</RouterLink>
+        <RouterLink :to="{ name: 'post-list' }">Posts</RouterLink>
+        <RouterLink :to="{ name: 'product-list' }">Shop</RouterLink>
       </div>
-      <VueRouterLink
+      <RouterLink
         to="/"
         title="To Home page"
-        class="flex w-full items-center aria-[current='page']:pointer-events-none md:max-w-[33%] md:justify-center"
+        variant="plain"
+        class="flex w-full items-center md:max-w-[33%] md:justify-center"
       >
         <img
           src="/assets/images/logo.svg"
@@ -35,7 +32,7 @@ const { isOpen, menuRef, toggle, close } = useHeaderMenu()
           ref="logoRef"
           class="w-full max-w-60 origin-center will-change-transform lg:max-w-75"
         />
-      </VueRouterLink>
+      </RouterLink>
       <div class="flex w-full items-center justify-end md:max-w-[33%] md:gap-4 lg:gap-10">
         <ButtonIcon title="Cart">
           <ShoppingBag />
