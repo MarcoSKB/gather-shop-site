130484cc9755063fef47fde9aa90b5626518f00c
Marco <serikbaev03beka@gmail.com>
2026-03-20 01:33:35 +0500

    refactor(ui): Update PostCard to use new props and formatting

--- src/entities/post/ui/PostCard.vue
+++ src/entities/post/ui/PostCard.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { BaseCard } from '@shared/ui/BaseCard'
+import type { RouteLocationRaw } from 'vue-router'
 import type { PreviewPost } from '../model/types'
 
 defineProps<{
-  to: string
+  to: RouteLocationRaw
   post: PreviewPost
 }>()
 </script>
@@ -12,19 +13,19 @@ defineProps<{
   <BaseCard :to class="font-spectral w-full gap-3">
     <template #image>
       <img
-        :src="post.image"
-        :alt="post.imageAlt"
+        :src="post.cover"
+        :alt="`Cover image for ${post.title.toLowerCase()}`"
         class="mb-3 w-full object-cover md:scale-105 md:transition-transform md:duration-250 md:ease-in md:group-hover:scale-100"
       />
       <span class="font-poppins mb-4 text-xs leading-3.75 font-light tracking-[10%]">
-        {{ post.subtitle }}
+        {{ post.category }}
       </span>
     </template>
     <template #content>
       <span class="text-xl leading-7 transition-opacity ease-in group-hover:opacity-70">
-        {{ post.title }}
+        {{ post.title.toLowerCase().replace(/^\w|\s\w/g, (c) => c.toUpperCase()) }}
       </span>
-      <span class="text-base leading-6 text-pretty opacity-80">{{ post.description }}</span>
+      <span class="text-base leading-6 text-pretty opacity-80">{{ post.subtitle }}</span>
     </template>
   </BaseCard>
 </template>
