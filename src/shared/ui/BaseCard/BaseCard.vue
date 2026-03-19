c9f3929da96c003134fad36dcc28e0b05a5d4a72
Marco <serikbaev03beka@gmail.com>
2026-03-20 01:33:20 +0500

    refactor(ui): Update image and content slots in BaseCard

--- src/shared/ui/BaseCard/BaseCard.vue
+++ src/shared/ui/BaseCard/BaseCard.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
 import clsx from 'clsx'
 import { computed } from 'vue'
-import { RouterLink } from 'vue-router'
+import { RouterLink, type RouteLocationRaw } from 'vue-router'
 
 const props = defineProps<{
   class?: string
-  to: string
+  to: RouteLocationRaw
 }>()
 
 const cardContentClasses = computed(() => clsx('flex flex-col', props.class))
 </script>
 
 <template>
-  <RouterLink :to class="group flex w-full flex-col">
-    <div class="relative z-0 w-full overflow-hidden">
-      <slot name="image" />
-    </div>
-    <div :class="cardContentClasses">
-      <slot name="content" />
-    </div>
-  </RouterLink>
+  <article>
+    <RouterLink :to class="group flex w-full flex-col">
+      <figure class="relative z-0 w-full overflow-hidden">
+        <slot name="image" />
+      </figure>
+      <div :class="cardContentClasses">
+        <slot name="content" />
+      </div>
+    </RouterLink>
+  </article>
 </template>
