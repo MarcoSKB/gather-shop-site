50ce948b0a43ed52e7969d736ed3bd27b2f3e738
Marco <serikbaev03beka@gmail.com>
2026-03-25 16:37:44 +0500

    feat(ui): Add post details and skeleton loading state

---
+++ src/pages/PostDetails/ui/PostDetails.skeleton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { BaseContainer, BaseSkeleton } from '@shared/ui'
+</script>
+
+<template>
+  <div
+    class="mx-auto flex h-[calc(100vh-64px)] max-w-480 flex-col-reverse md:h-[calc(100vh-90px)] md:max-h-300 md:min-h-197.5 md:flex-row"
+  >
+    <BaseContainer class="h-full w-full max-w-1/2 pt-6 pb-7.5 text-center md:pt-13.75">
+      <BaseSkeleton class="mx-auto mb-auto h-7.5 max-w-150 md:h-20 lg:h-32.5" />
+
+      <BaseSkeleton class="mx-auto mb-5 h-4.5 max-w-130 md:mb-11.75" />
+      <BaseSkeleton class="mx-auto h-3.5 max-w-[320px]" />
+    </BaseContainer>
+
+    <BaseSkeleton class="h-full w-full md:max-w-1/2" />
+  </div>
+</template>
--- src/pages/PostDetails/ui/PostDetailsPage.vue
+++ src/pages/PostDetails/ui/PostDetailsPage.vue
@@ -1,5 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { PostContent, usePost } from '@entities/post'
+import { UiState } from '@shared/ui'
+import { PostsGrid } from '@widgets/PostsGrid'
+import { TitledSection } from '@widgets/TitledSection'
+import PostDetailsSkeleton from './PostDetails.skeleton.vue'
+
+const { slug } = defineProps<{ slug: string }>()
+const { data: postDetails, uiStatus } = usePost(slug)
+</script>
 
 <template>
-  <section>PostDetails</section>
+  <UiState :status="uiStatus">
+    <template v-if="postDetails" #default>
+      <PostContent :blocks="postDetails.blocks" />
+      <TitledSection title="RELATED STORIES">
+        <PostsGrid
+          :params="{
+            categoryId: postDetails.category,
+            excludePostId: postDetails.id,
+          }"
+        />
+      </TitledSection>
+    </template>
+
+    <template #loading>
+      <PostDetailsSkeleton />
+    </template>
+  </UiState>
 </template>
