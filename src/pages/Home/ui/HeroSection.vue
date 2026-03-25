6d6382418fb30f4572d6a1bbfbf1254fd0da28f8
Marco <serikbaev03beka@gmail.com>
2026-03-25 16:30:32 +0500

    perf(ui): Update hero image format to webp

--- src/pages/Home/ui/HeroSection.vue
+++ src/pages/Home/ui/HeroSection.vue
@@ -7,9 +7,10 @@ import { BaseContainer } from '@shared/ui'
     class="relative z-0 flex h-full min-h-dvh flex-col items-center justify-center md:min-h-324 md:pt-24"
   >
     <img
-      src="/assets/images/hero-1.jpg"
+      src="/assets/images/hero-1.webp"
       alt="Watercolor painting with people"
       class="pointer-events-none absolute top-0 left-1/2 z-[-1] h-full w-full max-w-480 -translate-x-1/2 object-cover object-top md:pt-24"
+      fetchpriority="high"
     />
     <BaseContainer class="items-center justify-center gap-7 text-center">
       <h2
