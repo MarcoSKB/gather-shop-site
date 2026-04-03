3a85eae9d206d53a5e26a77461206fe2008c1894
Marco <serikbaev03beka@gmail.com>
2026-04-03 16:03:48 +0500

    feat(ui): Update product gallery with carousel and thumbnails

--- src/pages/ProductDetails/ui/ProductDetailsPage.vue
+++ src/pages/ProductDetails/ui/ProductDetailsPage.vue
@@ -17,14 +17,14 @@ const { data: product, uiStatus } = useProductBySlug(slug)
       <UiState :status="uiStatus">
         <template v-if="product" #default>
           <div
-            class="grid h-full grid-cols-1 gap-x-6 gap-y-6 md:grid-cols-[1fr_300px] md:gap-y-0 lg:grid-cols-[1fr_360px] lg:gap-x-10"
+            class="grid h-full grid-cols-1 items-start gap-x-6 gap-y-6 md:grid-cols-[1fr_300px] md:gap-y-0 lg:grid-cols-[1fr_360px] lg:gap-x-10"
           >
             <h1
               class="font-poppins text-[28px] leading-8 md:col-start-2 md:mb-2 lg:text-[38px] lg:leading-11"
             >
               {{ product.title }}
             </h1>
-            <ProductGallery :images="[product.previewImage]" />
+            <ProductGallery :images="product.images" />
             <ProductInfo :product="product" />
           </div>
         </template>
@@ -34,9 +34,10 @@ const { data: product, uiStatus } = useProductBySlug(slug)
             class="grid h-full grid-cols-1 gap-x-6 gap-y-6 md:grid-cols-[1fr_300px] md:gap-y-0 lg:grid-cols-[1fr_360px] lg:gap-x-10"
           >
             <BaseSkeleton class="h-7 max-w-[40%] md:col-start-2 md:mb-2 md:max-w-[70%] lg:h-10" />
-            <BaseSkeleton
-              class="row-span-2 aspect-video h-fit w-full md:col-start-1 md:row-start-1"
-            />
+            <div class="row-span-2 items-start md:col-start-1 md:row-start-1">
+              <BaseSkeleton class="mb-2.5 aspect-16/11 h-auto w-full" />
+              <BaseSkeleton class="h-20 w-full" />
+            </div>
             <div class="flex flex-col">
               <BaseSkeleton class="mb-3 h-4.5 max-w-[33%] md:mb-5 lg:mb-8" />
               <BaseSkeleton class="mb-8 h-87.5 max-w-[90%]" />
--- src/pages/ProductDetails/ui/ProductGallery.vue
+++ src/pages/ProductDetails/ui/ProductGallery.vue
@@ -1,13 +1,73 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { Carousel, Slide } from 'vue3-carousel'
+
 defineProps<{
   images: string[]
 }>()
+
+const currentSlide = ref(0)
+
+const slideTo = (nextSlide: number) => (currentSlide.value = nextSlide)
+
+const galleryConfig = {
+  transition: 1200,
+  transitionEasing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  itemsToShow: 1,
+  ignoreAnimations: true,
+  wrapAround: true,
+}
+
+const thumbnailsConfig = {
+  height: 80,
+  itemsToShow: 4,
+  touchDrag: false,
+  gap: 10,
+  breakpoints: {
+    768: {
+      itemsToShow: 5,
+    },
+    1024: {
+      itemsToShow: 6,
+    },
+  },
+}
 </script>
 
 <template>
-  <img
-    :src="images[0]"
-    alt="Product image"
-    class="row-span-2 aspect-video h-fit w-full object-cover md:col-start-1 md:row-start-1"
-  />
+  <div class="row-span-2 h-full max-w-full overflow-hidden md:col-start-1 md:row-start-1">
+    <Carousel v-bind="galleryConfig" v-model="currentSlide" class="mb-2.5 bg-gray-200">
+      <Slide
+        v-for="(image, idx) in images"
+        :key="image + idx"
+        class="relative z-0 aspect-16/11 h-auto w-full px-2 md:px-8 lg:px-20"
+      >
+        <img
+          :src="image"
+          alt="Product Image"
+          class="pointer-events-none absolute top-0 left-0 aspect-16/11 h-full w-full object-cover object-center"
+        />
+      </Slide>
+    </Carousel>
+
+    <Carousel v-bind="thumbnailsConfig" v-model="currentSlide">
+      <Slide v-for="(image, idx) in images" :key="image + idx">
+        <template #default="{ currentIndex, isActive }">
+          <div
+            @click="slideTo(currentIndex)"
+            :class="[
+              'aspect-16/11 h-auto w-full cursor-pointer opacity-60 transition-opacity duration-300 ease-in hover:opacity-100',
+              { 'opacity-100': isActive },
+            ]"
+          >
+            <img
+              :src="image"
+              alt="Thumbnail Image"
+              class="aspect-16/11 h-full w-full object-cover"
+            />
+          </div>
+        </template>
+      </Slide>
+    </Carousel>
+  </div>
 </template>
