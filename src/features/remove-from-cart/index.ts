25f36eccf563d06b6c172de747f0647fbb931498
Marco <serikbaev03beka@gmail.com>
2026-04-06 08:00:21 +0500

    feat(cart): Add "remove button" for cart items

---
+++ src/features/remove-from-cart/index.ts
@@ -0,0 +1,1 @@
+export { default as RemoveButton } from './ui/RemoveButton.vue'
---
+++ src/features/remove-from-cart/ui/RemoveButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useCartStore } from '@entities/cart'
+import type { CartItem } from '@entities/cart/model/types'
+
+const { id } = defineProps<{ id: CartItem['id'] }>()
+
+const cartStore = useCartStore()
+
+const removeFromCart = () => cartStore.remove(id)
+</script>
+
+<template>
+  <button
+    class="font-poppins cursor-pointer text-left font-thin transition-opacity ease-in hover:opacity-80"
+    @click.stop="removeFromCart"
+  >
+    Remove
+  </button>
+</template>
