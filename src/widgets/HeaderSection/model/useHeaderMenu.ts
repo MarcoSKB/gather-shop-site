4cc1f856b798677ab76fc67359609a811db4865f
Marco <serikbaev03beka@gmail.com>
2026-04-06 18:09:32 +0500

    perf(core): Optimize scroll handling with hook

--- src/widgets/HeaderSection/model/useHeaderMenu.ts
+++ src/widgets/HeaderSection/model/useHeaderMenu.ts
@@ -1,23 +1,13 @@
+import { useLockScroll } from '@shared/lib/hooks'
 import gsap from 'gsap'
 import { nextTick, onUnmounted, ref } from 'vue'
 
-export function useHeaderMenu() {
+export const useHeaderMenu = () => {
   const isOpen = ref(false)
   const menuRef = ref<HTMLElement | null>(null)
+  const { lockScroll, unlockScroll } = useLockScroll()
 
   let tl: gsap.core.Timeline | null = null
-
-  const lockScroll = () => {
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
-
-    document.body.style.overflow = 'hidden'
-    document.body.style.paddingRight = `${scrollbarWidth}px`
-  }
-
-  const unlockScroll = () => {
-    document.body.style.overflow = ''
-    document.body.style.paddingRight = ''
-  }
 
   const open = async () => {
     isOpen.value = true
