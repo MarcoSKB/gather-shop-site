1c615577121fd08098dacbf18d0d82891044641a
Marco <serikbaev03beka@gmail.com>
2026-04-11 22:41:55 +0500

    refactor(ui): Remove custom animation logic

--- src/pages/PostList/ui/PostListPage.vue
+++ src/pages/PostList/ui/PostListPage.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { PostCard, PostCardSkeleton, usePostsList } from '@entities/post'
+import { useStaggerAppear } from '@shared/lib'
 import { BaseContainer, UiState } from '@shared/ui'
 import { ref, watch } from 'vue'
-import { usePostAppearAnimation } from '../lib/usePostAppearAnimation'
 
-const containerRef = ref<HTMLElement | null>(null)
+const listRef = ref<HTMLElement | null>(null)
 
-const animate = usePostAppearAnimation(containerRef)
 const { data: posts, uiStatus } = usePostsList({ take: 12 })
+const { animate } = useStaggerAppear(listRef, '.post-item')
 
 watch(
-  posts,
+  () => posts.value,
   async (newVal) => {
     if (newVal?.length) {
       await animate()
@@ -25,10 +25,10 @@ watch(
     <BaseContainer>
       <UiState :status="uiStatus">
         <ul
-          ref="containerRef"
+          ref="listRef"
           class="grid grid-cols-2 gap-x-3 gap-y-6 py-5 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-5 md:gap-y-10 md:py-8 lg:gap-x-10 lg:gap-y-14 lg:py-12"
         >
-          <li v-for="post in posts" :key="post.id">
+          <li v-for="post in posts" :key="post.id" class="post-item">
             <PostCard :post="post" />
           </li>
         </ul>
