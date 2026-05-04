7654e1c4d3fd2df76eab539a512414e563bdd24f
Marco <serikbaev03beka@gmail.com>
2026-05-05 00:10:28 +0500

    refactor(ui): Update input size variants and default size

--- src/shared/ui/BaseInput/BaseInput.styles.ts
+++ src/shared/ui/BaseInput/BaseInput.styles.ts
@@ -5,13 +5,15 @@ export const inputStyles = cva('font-poppins w-full', {
     variant: {
       primary:
         'placeholder:text-primary border-primary border-b border-solid uppercase placeholder:uppercase focus:outline-0 focus:placeholder:opacity-50',
+      secondary: 'border-primary placeholder:text-primary border',
     },
     size: {
-      md: 'py-1.25 text-xs tracking-[10%]',
-      lg: 'text-md py-5 leading-5.5 tracking-[2%]',
+      sm: 'py-1.25 text-xs tracking-[10%]',
+      md: 'text-md py-5 leading-5.5 tracking-[2%]',
+      lg: 'placeholder:text-primary/70 px-3 py-2 text-lg leading-6.25 placeholder:text-lg',
     },
   },
-  defaultVariants: { variant: 'primary', size: 'md' },
+  defaultVariants: { variant: 'primary', size: 'sm' },
 })
 
 export type InputVariantsType = VariantProps<typeof inputStyles>
